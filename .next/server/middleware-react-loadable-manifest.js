792343bdc32c6f9c796541cb720f1773f0a5abb2
AdemiluaIbukun <ademiluatise@gmail.com>
2024-06-11 11:30:06 +0100

    Resolving Conflicts

--- .next/server/middleware-react-loadable-manifest.js
+++ .next/server/middleware-react-loadable-manifest.js
@@ -1,1 +1,1 @@
-self.__REACT_LOADABLE_MANIFEST="{\"index.js -> ../src/modules/HomeOne\":{\"id\":\"index.js -> ../src/modules/HomeOne\",\"files\":[\"static/chunks/src_modules_HomeOne_index_js.js\"]},\"services/[slug]/index.js -> ../../../src/modules/ServiceDetails\":{\"id\":\"services/[slug]/index.js -> ../../../src/modules/ServiceDetails\",\"files\":[\"static/chunks/src_modules_ServiceDetails_index_js.js\"]}}"
+self.__REACT_LOADABLE_MANIFEST="{}"
