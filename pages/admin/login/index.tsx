ff55fe21b02dd4b43a5dace91867ca64d385d948
Ibukunoluwa <ademiluatise@gmail.com>
2024-06-12 13:20:29 +0100

    Added Admin Sign In and Dashboard

---
+++ pages/admin/login/index.tsx
@@ -0,0 +1,16 @@
+import Footer from "../../../src/components/Footer";
+import HeaderTwo from "../../../src/components/Header/HeaderTwo";
+import Preloader from "../../../src/components/Preloader";
+import dynamic from "next/dynamic";
+
+const AdminSignInModules = dynamic(() => import("../../../src/modules/AdminSignin"), {
+  loading: () => <Preloader />,
+});
+
+export default function SignIn() {
+  return (
+    <main>
+      <AdminSignInModules />
+    </main>
+  );
+}
---
+++ src/modules/AdminSignin/index.tsx
@@ -0,0 +1,125 @@
+import React, { useEffect, useState } from "react";
+import { useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
+import * as yup from "yup";
+import { useRouter } from "next/navigation";
+import { useDispatch, useSelector } from "react-redux";
+import { ThunkDispatch, UnknownAction } from "@reduxjs/toolkit";
+import { RootState } from "../../Globals/store/store";
+import { loginAdmin } from "../../Globals/Slices/AuthSlice/LoginAdminSlice";
+import LoaderSpinner from "../../components/Spinner";
+
+// Validation schema
+const schema = yup.object().shape({
+  username: yup.string().required("Username is required"),
+  password: yup.string().required("Password is required"),
+});
+
+type AppDispatch = ThunkDispatch<RootState, unknown, UnknownAction>;
+
+export default function AdminSignIn() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    clearErrors,
+  } = useForm({
+    resolver: yupResolver(schema),
+  });
+  const dispatch: AppDispatch = useDispatch();
+  const router = useRouter();
+
+  const loading = useSelector((state: RootState) => state?.loginAdmin?.loading);
+  const success = useSelector((state: RootState) => state?.loginAdmin?.success);
+
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  const onSubmit = async () => {
+    await dispatch(
+      loginAdmin({
+        username,
+        password,
+      })
+    );
+  
+  };
+
+  useEffect(() => {
+    if (success) {
+      router.push("/admin/dashboard");
+    }
+  }, [success]);
+
+
+  const handleUsernameChange = (e: any) => {
+    setUsername(e.target.value);
+    clearErrors("username");
+  };
+
+  const handlePasswordChange = (e: any) => {
+    setPassword(e.target.value);
+    clearErrors("password");
+  };
+
+  return (
+    <div className="it-signup-area pt-120 pb-120">
+      <div className="container w-50">
+        <div className="it-signup-bg p-relative">
+          <div className="row ">
+            <div className="col-xl-12 col-lg-12">
+              <form onSubmit={handleSubmit(onSubmit)}>
+                <div className="it-signup-wrap">
+                  <h4 className="it-signup-title text-center">Admin Sign In</h4>
+                  <div className="it-signup-input-wrap">
+                    <div className="it-signup-input mb-20">
+                      <input
+                        type="text"
+                        placeholder="Username *"
+                        {...register("username")}
+                        value={username}
+                        onChange={handleUsernameChange}
+                      />
+                      {errors.username && (
+                        <p style={{ color: "red" }}>
+                          {errors.username.message}
+                        </p>
+                      )}
+                    </div>
+                    <div className="it-signup-input mb-20">
+                      <input
+                        type="password"
+                        placeholder="Password *"
+                        {...register("password")}
+                        value={password}
+                        onChange={handlePasswordChange}
+                      />
+                      {errors.password && (
+                        <p style={{ color: "red" }}>
+                          {errors.password.message}
+                        </p>
+                      )}
+                    </div>
+                  </div>
+                  {loading ? (
+                    <div className="it-signup-btn mb-40">
+                      <button className="it-btn large" type="submit">
+                        <LoaderSpinner />
+                      </button>
+                    </div>
+                  ) : (
+                    <div className="it-signup-btn mb-40 text-center">
+                      <button className="it-btn large" type="submit">
+                        Log In
+                      </button>
+                    </div>
+                  )}
+                </div>
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
