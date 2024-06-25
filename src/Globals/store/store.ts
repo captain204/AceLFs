10d644049a466de2b36fd63d09cb1b454f9828cc
captain204 <nurudeenakindele8@gmail.com>
2024-06-25 07:52:42 +0100

    Store Conflict Resolve

--- src/Globals/store/store.ts
+++ src/Globals/store/store.ts
@@ -5,13 +5,6 @@ import loginUserReducer from "../Slices/AuthSlice/LoginUserSlice";
 
 import degreesReducer from "../Slices/Degree/DegreesSlice";
 import coursesReducer from "../Slices/Degree/CoursesSlice";
-<<<<<<< HEAD
-import  emergencyFormReducer from "../Slices/ApplicationSlice/Emergency"
-import  submitRefereeFormReducer from "../Slices/ApplicationSlice/Referee"
-import submitFirstFormReducer from "../Slices/ApplicationSlice/FirstFormSlice"
-import newsFormReducer from "../Slices/ApplicationSlice/News"
-import loginAdminReducer from '../Slices/AuthSlice/LoginAdminSlice'
-=======
 import emergencyFormReducer from "../Slices/ApplicationSlice/Emergency";
 import submitRefereeFormReducer from "../Slices/ApplicationSlice/Referee";
 import submitFirstFormReducer from "../Slices/ApplicationSlice/FirstFormSlice";
@@ -20,7 +13,6 @@ import loginAdminReducer from "../Slices/AuthSlice/LoginAdminSlice";
 import applicantsReducer from "../Slices/AdminSlices/ApplicantsSlice";
 import ApproveadmissionReducer from "../Slices/AdminSlices/ApproveAdmission";
 import degreeTypeApplicantsReducer from "../Slices/AdminSlices/DegreeTypeApplicants"
->>>>>>> 1a773322d4179ea2a42d5e97c7c0bd953fef62f0
 
 
 const rootReducer = combineReducers({
@@ -32,15 +24,10 @@ const rootReducer = combineReducers({
   emergency: emergencyFormReducer,
   referee: submitRefereeFormReducer,
   personal: submitFirstFormReducer,
-<<<<<<< HEAD
-  newsForm: newsFormReducer
-=======
   newsForm: newsFormReducer,
-  loginAdmin: loginAdminReducer,
   applicants: applicantsReducer,
   approve: ApproveadmissionReducer,
   degreetypeapplicants: degreeTypeApplicantsReducer
->>>>>>> 1a773322d4179ea2a42d5e97c7c0bd953fef62f0
 });
 
 export const store = configureStore({
